f2bf9b1e3819c50831598017e9ec3d93b7ab78d2
vagrant <vagrant@homestead>
2018-10-23 08:15:49 +0000

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//主页
+Route::get('/', 'StaticPagesController@home');
+//帮助页
+Route::get('/help', 'StaticPagesController@help');
+//关于页
+Route::get('/about', 'StaticPagesController@about');
 
-Route::get('/', function () {
-    return view('welcome');
-});
